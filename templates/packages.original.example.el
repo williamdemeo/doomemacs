6a68366401dc1fad23ee9355f62dd3441fb12e26
William DeMeo <williamdemeo@gmail.com>
2023-03-30 23:38:12 -0400

    feat: add warning for custom fork configs

--- templates/packages.original.example.el
+++ templates/packages.original.example.el
@@ -7,19 +7,13 @@
 
 
 ;; To install SOME-PACKAGE from MELPA, ELPA or emacsmirror:
-(package! ag)
+;(package! some-package)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
 ;; https://github.com/radian-software/straight.el#the-recipe-format
 ;(package! another-package
 ;  :recipe (:host github :repo "username/repo"))
-
-;; (package! blamer)
-;; or
-(package! blamer :recipe (:host github :repo "artawower/blamer.el"))
-
-
 
 ;; If the package you are trying to install does not contain a PACKAGENAME.el
 ;; file, or is located in a subdirectory of the repo, you'll need to specify
